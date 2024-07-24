3a3e74040c5d1a30c4978aa4e2149d4b0b283371
Shambhuraj11 <shambhurajpatil11@gmail.com>
2024-07-24 06:07:26 +0530

    Updated everything

--- setup.py
+++ setup.py
@@ -6,11 +6,24 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+__version__ = "0.0.1"
+REPO_NAME = "python_package"
+PKG_NAME= "dbautomation"
+AUTHOR_USER_NAME = "Shambhuraj11"
+AUTHOR_EMAIL = "shambhurajpatil11@gmail.com"
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirement(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
     name=PKG_NAME,
@@ -26,4 +39,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires = get_requirement("./requirements_dev.txt")
     )
