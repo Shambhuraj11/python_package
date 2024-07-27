3657fca4bc6a160101543079908570f6bac405ed
Shambhuraj11 <shambhurajpatil11@gmail.com>
2024-07-27 11:13:06 +0530

    project toml and setup file updated

--- setup.py
+++ setup.py
@@ -3,14 +3,25 @@ from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
-   
-
+    
 __version__ = "0.0.2"
 REPO_NAME = "python_package"
 PKG_NAME= "dbautomation"
 AUTHOR_USER_NAME = "Shambhuraj11"
 AUTHOR_EMAIL = "shambhurajpatil11@gmail.com"
 
+HYPEN_E_DOT = "-e ."
+
+def get_requirement(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
     name=PKG_NAME,
@@ -26,4 +37,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires = get_requirement("requirements.txt")
     )
