3b6a4198ee8444e21e7b7fcb29cc504767d99502
Shambhuraj11 <shambhurajpatil11@gmail.com>
2024-07-27 11:18:09 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -37,5 +37,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirement("requirements.txt")
+    install_requires = ["pymongo","pymongo[srv]","dnspython","pandas","numpy","ensure","pytest"]
     )
