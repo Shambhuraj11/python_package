ea432a4aee256d7661e4c6e6d02c3a5a60446d74
Shambhuraj11 <shambhurajpatil11@gmail.com>
2024-07-27 10:29:45 +0530

    crud file updated

--- src/dbautomation/mongo_crud.py
+++ src/dbautomation/mongo_crud.py
@@ -7,45 +7,45 @@ import json
 
 class mongo_operation:
 
-    def __init__(self,client_url: str, database_name: str, collection_name: str=None):
-        self.client_url=client_url
-        self.database_name=database_name
-        self.collection_name=collection_name
+    def __init__(self, client_url: str, database_name: str, collection_name: str):
+        self.client_url = client_url
+        self.database_name = database_name
+        self.collection_name = collection_name
 
     def create_client(self):
-        client=MongoClient(self.client_url)
+        client = MongoClient(self.client_url)
         return client
 
     def create_database(self):
-        client=self.create_client()
-        database=client[self.database_name]
+        client = self.create_client()
+        database = client[self.database_name]
         return database
         
-    def create_collection(self,collection=None):
-        database=self.create_database()
-        collection=database[collection]
+    def create_collection(self,collection:str):
+        database = self.create_database()
+        collection = database[collection]
         return collection
 
     def insert_record(self,record:Union[dict,list],collection_name:str) -> Any: 
-        if type(record)==list:
+        if type(record) == list:
             for data in record:
-                if type(data)!= dict:
+                if type(data) != dict:
                     raise TypeError("record must be in the dict")
-            collection=self.create_collection(collection_name)
+            collection = self.create_collection(collection_name)
             collection.insert_many(record)
-        elif type(record)==dict:
-            collection=self.create_collection(collection_name)
+        elif type(record) == dict:
+            collection = self.create_collection(collection_name)
             collection.insert_one(record)
 
-    def bulk_insert(self,datafile:str,collection_name:str=None):
-        self.path=datafile
+    def bulk_insert(self,datafile:str,collection_name:str):
+        self.path = datafile
 
         if self.path.endswith('.csv'):
-            data=pd.read_csv(self.path,encoding='utf-8')
+            data = pd.read_csv(self.path,encoding='utf-8')
 
         elif self.path.endswith('.xlsx'):
-            data=pd.read_excel(self.path,encoding='utf-8')
+            data = pd.read_excel(self.path,encoding='utf-8')
             
-        datajson=json.loads(data.to_json(orient='record'))
-        collection=self.create_collection()
+        datajson = json.loads(data.to_json(orient='record'))
+        collection = self.create_collection(collection_name)
         collection.insert_many(datajson)
